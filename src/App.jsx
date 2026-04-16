bf4aabb4172c8f533c654087b8033469d3bdcce4
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2026-04-16 20:13:41 +0600

    homepage design complete

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import Hero from './components/hero/Hero';
+import Navbar from './components/navbar/Navbar';
+import Products from './components/products/Products';
+import Sidebar from './components/sidebar/Sidebar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <main className="max-w-6xl mx-auto px-4 py-12 space-y-10">
+        <Hero />
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+          <Sidebar />
+          <Products />
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
